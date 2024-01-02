ba39a64c83cad21cd5da7fd5e104bc8ea312ef5d
Maria Dyachko <up_11_14@mail.ru>
2024-01-02 20:49:52 +0300

    first coomit

---
+++ 01_web/http-server/src/main/java/ru/netology/Main.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+
+      //localhost:9999/spring.svg
+
+      Server server = new Server();
+      server.start();
+
+  }
+}
+
