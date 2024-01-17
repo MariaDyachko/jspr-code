ddcd1ad749b6d3063bd2cd3743ab3574af41fbac
Maria Dyachko <up_11_14@mail.ru>
2024-01-18 00:56:00 +0300

    new relese

---
+++ web_02_forms/src/main/java/org/example/Main.java
@@ -0,0 +1,54 @@
+package org.example;
+import java.io.*;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final var server = new Server( 9998, 64);
+
+//        server.addHandler("GET", "/index.html", (request, out) -> {
+//            try {
+//                var filePath = Path.of(".", "public", request.path());
+//                var mimeType = Files.probeContentType(filePath);
+//                var sizeFile = Files.size(filePath);
+//                outResponse(mimeType, sizeFile, out, filePath);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+        server.addHandler("GET", "/spring.png", (request, out) -> {
+            try {
+                var filePath = Path.of(".", "public", request.path());
+                var mimeType = Files.probeContentType(filePath);
+                var sizeFile = Files.size(filePath);
+                outResponse(mimeType, sizeFile, out, filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        server.addHandler("POST", "/messages", (request, out) -> {
+            try {
+                Path filePath = Path.of(".", "public", request.path());
+                String mimeType = Files.probeContentType(filePath);
+                long sizeFile = Files.size(filePath);
+                outResponse(mimeType, sizeFile, out, filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        server.startedServer(9998);
+    }
+    private static void outResponse(String mimeType, long size, BufferedOutputStream bufferedOutputStream, Path path) throws IOException {
+        bufferedOutputStream.write((
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + size + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        Files.copy(path, bufferedOutputStream);
+        bufferedOutputStream.flush();
+    }
+}
+
+
